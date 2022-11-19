0b9785c2783d794ebc46aaab4567995a2ec3e3f2
Tony Lim <tonylim9438@gmail.com>
2022-11-19 12:36:22 -0800

    signup component finished for now Co-authored-by: Linda Ngo <lindango142@gmail.com> Co-authored-by: Tony Lim <tonylim9438@gmail.com>

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
 import { hot } from 'react-hot-loader/root';
+import Signup from './components/Signup.jsx'
 
 const App = () => {
 
     return (
         <div className="">
             <p className="text-2xl bg-red-300">Hello! We are setting up a real world application!   </p>
+            <Signup/> 
         </div>
     )
 }
